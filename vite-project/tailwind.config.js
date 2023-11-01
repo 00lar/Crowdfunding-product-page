e5c1a71c21e55fbb3c39b6f9bdada33f0353ed3c
00lar <eduardani84@gmail.com>
2023-10-31 23:47:50 -0600

    I have given styles to my app

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Primary1": "hsl(176, 50%, 47%)",
+        "Primary2": "hsl(176, 72%, 28%)",
+        "Neutral1": "hsl(0, 0%, 0%)",
+        "Neutral2": "hsl(0, 0%, 48%)"
+      }
+    },
   },
   plugins: [],
 }
